7f86299e85551f8ff323ecc9e608e3fe3bcf43d1
deliriusz <deliriusz.eth@gmail.com>
2025-07-10 11:44:18 +0200

    adding node exporter installation

---
+++ install_node_exporter.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -x
+
+cd /tmp
+wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
+tar xfz node_exporter-1.9.1.linux-amd64.tar.gz
+mv node_exporter-1.9.1.linux-amd64 /opt/node_exporter
+rm node_exporter-1.9.1.linux-amd64.tar.gz
+chmod 755 /opt/node_exporter/node_exporter
+
+cat <<EOF > /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Prometheus Server node exporter
+Documentation=https://prometheus.io/docs/introduction/overview/
+After=network-online.target
+
+[Service]
+User=root
+Restart=on-failure
+
+ExecStart=/opt/node_exporter/node_exporter --web.listen-address=:30164
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable node_exporter
+systemctl start node_exporter
+systemctl status node_exporter
